c04cd28bb3e330b60214536540b2e350b451bc37
Jack <jack@nimbusvapour.com.au>
2022-09-04 20:48:45 +1000

    Added mobile menu transition

--- src/scripts.js
+++ src/scripts.js
@@ -1,4 +1,18 @@
 function handleMenuToggle() {
   const menu = document.getElementById("mobile-menu");
-  menu.classList.toggle("hidden");
+  const menuCont = document.getElementById("mobile-menu-cont");
+
+  if (menu.classList.contains("hidden")) {
+    menu.classList.remove("hidden");
+    setTimeout(() => {
+      menuCont.classList.remove("opacity-0");
+      menuCont.classList.add("opacity-100");
+    }, 1);
+  } else {
+    menuCont.classList.remove("opacity-100");
+    menuCont.classList.add("opacity-0");
+    setTimeout(() => {
+      menu.classList.add("hidden");
+    }, 200);
+  }
 }
