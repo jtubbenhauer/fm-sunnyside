4b11ebb7bc73b0574b68e0b8fcdee0018e0c52be
Jack <jack@nimbusvapour.com.au>
2022-09-04 14:59:53 +1000

    Added fonts and colour variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import './App.css'
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-
   return (
-   <div></div>
-  )
+    <>
+      <Navbar />
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./scss/index.scss";
+import "@fontsource/barlow/600.css";
+import "@fontsource/fraunces/700.css";
+import "@fontsource/fraunces/900.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
